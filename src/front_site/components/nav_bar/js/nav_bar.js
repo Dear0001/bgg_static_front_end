e022785e15cca987cf7c6f69ce55cf76dcfd826f
sotsirymony <monysout69@gmail.com>
2023-06-03 16:42:01 +0700

    updated javascript

--- src/front_site/components/nav_bar/js/nav_bar.js
+++ src/front_site/components/nav_bar/js/nav_bar.js
@@ -1,19 +1,29 @@
-//jquery dropdown
-// $(document).ready(function(){
-//     $(".dropdown").hover(function(){
-//         var dropdownMenu = $(this).children(".dropdown-menu");
-//         if(dropdownMenu.is(":visible")){
-//             dropdownMenu.parent().toggleClass("open");
-//         }
-//     });
-// }); 
-
 
 //drodown animation jquery test
-
-
  // jquery with for search bar mobile
  $(".fa").click(function(){
     $(".icon").toggleClass("active");
     $("input[type=text]").toggleClass("active");
 })
+//scroll to the top hide icon
+jQuery(document).ready(function() { 
+    fadeMenuWrap(); 
+    jQuery(window).scroll(fadeMenuWrap);
+});
+function fadeMenuWrap() { 
+    var scrollPos = window.pageYOffset || document.documentElement.scrollTop; 
+    if(scrollPos==0){
+        $(".icon").removeClass("hide_icon");
+        $(".icon").removeClass("sticky");
+        $(".icon-title-wrapper").removeClass("rePrepare");
+    }
+    else if (scrollPos > 300) { 
+        $('.nav_wrapper').addClass('sticky')
+        $(".icon").addClass("hide_icon");
+        $(".icon-title-wrapper").addClass("rePrepare");
+    } else { 
+        $('.nav_wrapper').addClass('sticky')
+        $(".icon").addClass("hide_icon");
+        $(".icon-title-wrapper").addClass("rePrepare");
+    } 
+} 
